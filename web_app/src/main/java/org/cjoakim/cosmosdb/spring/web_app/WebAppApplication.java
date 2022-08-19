b89ce6c0ccb562ff88342d2381edb1d33e8c66cf
Chris Joakim <christopher.joakim@gmail.com>
2022-08-19 11:28:23 -0400

    Added the console_app/ and web_app/ dirs from Intellij Spring Initializr

---
+++ web_app/src/main/java/org/cjoakim/cosmosdb/spring/web_app/WebAppApplication.java
@@ -0,0 +1,13 @@
+package org.cjoakim.cosmosdb.spring.web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
+
+}
