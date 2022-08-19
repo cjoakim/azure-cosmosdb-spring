6d9b1f213537bb87efc7fedbf3b1936bb6b7b391
Chris Joakim <christopher.joakim@gmail.com>
2022-08-19 17:27:39 -0400

    Added EpaOzoneTelemetryRepository.java

---
+++ console_app/src/main/java/org/cjoakim/cosmos/spring/repository/EpaOzoneTelemetryRepository.java
@@ -0,0 +1,23 @@
+package org.cjoakim.cosmos.spring.repository;
+
+import com.azure.spring.data.cosmos.repository.CosmosRepository;
+import org.cjoakim.cosmos.spring.model.EpaOzoneTelemetryEvent;
+import org.cjoakim.cosmos.spring.model.Library;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data Repository for class EpaOzoneTelemetryEvent.
+ *
+ * Chris Joakim, Microsoft, August 2022
+ */
+
+@Component
+@Repository
+public interface EpaOzoneTelemetryRepository extends CosmosRepository<EpaOzoneTelemetryEvent, String> {
+
+//    Iterable<Library> findByPkAndTenant(String pk, String tenant);
+//
+//    Iterable<Library> findByPkAndTenantAndDoctype(String pk, String tenant, String doctype);
+
+}
